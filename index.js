ed11493aa46d86ecc342dddabebfca260aefd359
Jul Lop <lopcaljulio@gmail.com>
2023-11-26 12:51:29 +0100

    Modificacion

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const sum = require('./sum')
-console.log(sum(1,3))
+const os = require('os');
+const sum = require('./sum');
+console.log(sum(1, 3));
+
+console.log('Información del Sistema');
+console.log('-----------------------');
+console.log('Sistema operativo : ', os.platform()); // Corrected: os.platform()
+console.log('Versión del sistema : ', os.release());
+console.log('Arquitectura : ', os.arch());
+console.log('CPUs : ', os.cpus());
+console.log('Memoria libre : ', os.freemem() / 1024 / 1024);
+console.log('Memoria Total :  ', os.totalmem() / 1024 / 1024);
 
